e81d66f30f390d2701cdf4a451d95725579a27b0
Doanh <doanhhnqt74@gmail.com>
2017-12-28 20:16:27 +0700

    first commit

---
+++ BackgroundWorkerViblo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BackgroundWorkerViblo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.myBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(68, 126);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(335, 39);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(68, 195);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(124, 23);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(279, 195);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(124, 23);
+            this.btStop.TabIndex = 1;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(192, 92);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(76, 13);
+            this.lbStatus.TabIndex = 2;
+            this.lbStatus.Text = "Processing 0%";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(119, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Background Woker";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 265);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "MainForm";
+            this.Text = "Viblo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Label lbStatus;
+        private System.ComponentModel.BackgroundWorker myBackgroundWorker;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BackgroundWorkerViblo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerViblo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            myBackgroundWorker.DoWork += myBackgroundWorker_DoWork;
+            myBackgroundWorker.ProgressChanged += myBackgroundWorker_ProgressChanged;
+            myBackgroundWorker.RunWorkerCompleted += myBackgroundWorker_RunWorkerCompleted;
+
+            myBackgroundWorker.WorkerReportsProgress = true;
+            myBackgroundWorker.WorkerSupportsCancellation = true;
+
+            btStart.Enabled = true;
+            btStop.Enabled = false;
+        }
+
+        void myBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbStatus.Text = "Task Cancelled.";
+            }
+
+            else if (e.Error != null)
+            {
+                lbStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                // Everything completed normally.
+                lbStatus.Text = "Task Completed...";
+            }
+
+            //Change the status of the buttons on the UI accordingly
+            btStart.Enabled = true;
+            btStop.Enabled = false;
+        }
+
+        void myBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lbStatus.Text = "Processing......" + progressBar1.Value.ToString() + "%";
+        }
+
+        void myBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+
+                myBackgroundWorker.ReportProgress(i);
+
+                if (myBackgroundWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    myBackgroundWorker.ReportProgress(0);
+                    return;
+                }
+            }
+            
+            myBackgroundWorker.ReportProgress(100);
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            btStart.Enabled = false;
+            btStop.Enabled = true;
+
+            myBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            if (myBackgroundWorker.IsBusy)
+            {
+                myBackgroundWorker.CancelAsync();
+            }
+        }
+    }
+}
